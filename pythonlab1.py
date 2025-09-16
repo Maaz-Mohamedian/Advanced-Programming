cbd8c1babb5e0ad88331a239ff2b7319e15194a4
Maaz-Mohamedian <114775074+Maaz-Mohamedian@users.noreply.github.com>
2025-09-16 13:49:24 +0100

    Update pythonlab1.py

--- pythonlab1.py
+++ pythonlab1.py
@@ -1,6 +1,6 @@
 try: 
    a = float(input("Enter first number: "))
-   b = float(input("Enter secind number: "))
+   b = float(input("Enter second number: "))
 
   print(f"Sum: {a + b} ")
   print(f"difference: {a - b} ")
