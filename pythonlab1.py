78d697529d4afc012a2d18666869246ffa78c5a2
Maaz-Mohamedian <114775074+Maaz-Mohamedian@users.noreply.github.com>
2025-09-16 13:42:56 +0100

    Create pythonlab1.py

---
+++ pythonlab1.py
@@ -0,0 +1,15 @@
+try: 
+   a = float(input("Enter first number: "))
+  b = float(input("Enter secind number: "))
+
+  print(f"Sum: {a + b} ")
+  print(f"difference: {a - b} ")
+  print(f"product: {a * b} ")
+
+  if b != 0:
+     print(f"Division: {a / b:.2f}")
+  else:
+     print("Error: Division by zero is not allowed.")
+
+except ValueError:
+    print("invalid input. Plases enter numeric values.")
