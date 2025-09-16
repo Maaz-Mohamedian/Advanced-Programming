7631bd589463c838cadc6118ea8ac252d7563fdc
Maaz-Mohamedian <114775074+Maaz-Mohamedian@users.noreply.github.com>
2025-09-16 14:51:44 +0100

    Update pythonlab1.py

--- pythonlab1.py
+++ pythonlab1.py
@@ -13,3 +13,13 @@ try:
 
 except ValueError:
     print("invalid input. Plases enter numeric values.")
+
+numbers = [12, 5, 8, 22, 7, 19, 30, 4, 10, 16]
+
+print("Largest:", max(numbers))
+print("Smallest:", min(numbers))
+print("Average:", sum(numbers) / len(numbers))
+
+num_set = set(numbers)
+print("Orignal List:", numbers)
+print("Converted set:", num_set)
